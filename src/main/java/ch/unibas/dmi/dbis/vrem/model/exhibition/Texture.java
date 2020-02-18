c408c97d9d453f74bc0693b094ee9469b1c18ea6
Loris Sauter <loris.sauter@unibas.ch>
2020-02-18 14:52:22 +0100

    Removed legacy java code

--- src/main/java/ch/unibas/dmi/dbis/vrem/model/exhibition/Texture.java
+++
@@ -1,5 +0,0 @@
-package ch.unibas.dmi.dbis.vrem.model.exhibition;
-
-public enum Texture {
-    NONE, WOOD1, WOOD2, WOOD3, WALLPAPER, BRICKS, STARS, CONCRETE, CONCRETETILES, FABRIC, GOLD, LOUISXIV, SCRATCHES, SILVER, TILES
-}
