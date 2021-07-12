2194edaa2e8979d7a0054c6786acc46f5e69e4e4
Simon Peterhans <simon.peterhans@unibas.ch>
2021-07-12 17:27:16 +0200

    Update git and build

--- launch_db.sh
+++ launch_db.sh
@@ -5,20 +5,23 @@
 containers=$(docker ps -al | grep "vrem-container")
 
 if [ -z "$containers" ]; then
-    # Variable empty: Run new container.
-	echo "No vrem-container found, creating new one."
-	docker run --name vrem-container -d -p 27017:27017 mongo
-	echo "Started new container."
-else
-    # Container exists.
-    if [[ $containers == *Exited* ]]; then
-        # Stopped, restart.
-		echo "Existing container was stopped. Restarting."
-		docker start "vrem-container"
-	    echo "Restarted container."
-	else
-	    # Already running, do nothing.
-		echo "vrem-container already running."
-	fi
-fi
+  # Variable empty: Run new container.
+  echo "No vrem-container found, creating new one."
+  docker run --name vrem-container -d -p 27017:27017 mongo
+  echo "Started new container."
 
+else
+  # Container exists.
+  if [[ $containers == *Exited* ]]; then
+    # Stopped, restart.
+    echo "Existing container was stopped. Restarting."
+    docker start "vrem-container"
+    echo "Restarted container."
+
+  else
+    # Already running, do nothing.
+    echo "vrem-container already running."
+
+  fi
+
+fi
