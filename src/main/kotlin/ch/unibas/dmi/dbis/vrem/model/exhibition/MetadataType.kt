231ae89826b080526b36ecff350470967febdd50
Simon Peterhans <simon.peterhans@gmail.com>
2021-09-30 23:54:14 +0200

    Remove unnecessary metadata type, update kotlinx

--- src/main/kotlin/ch/unibas/dmi/dbis/vrem/model/exhibition/MetadataType.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/vrem/model/exhibition/MetadataType.kt
@@ -3,7 +3,6 @@ package ch.unibas.dmi.dbis.vrem.model.exhibition
 enum class MetadataType(val key: String = "") {
 
     GENERATED("generated"),
-    PREDECESSOR("predecessor"),
     OBJECT_ID("objectId"),
     MEMBER_IDS("memberIds"),
     SEED("seed")
